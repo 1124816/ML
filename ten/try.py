e7bf3b1d00345d62d96ae74809bfaca4f881f199
Elias Basler <e.e.basler@gmail.com>
2017-05-11 15:02:03 -0400

    Switch to keras

--- ten/try.py
+++ ten/try.py
@@ -1,9 +1,10 @@
 import gym
 import random
 import numpy as np
-import tflearn
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
+from keras.optimizers import SGD
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import Dense, Dropout, Activation
 from statistics import median, mean
 from collections import Counter
 
@@ -69,32 +70,37 @@ def initial_population():
     return training_data
 
 def neural_network_model(input_size):
-    network = input_data(shape=[None, input_size, 1], name ='input')
+    model = Sequential()
+    #network = input_data(shape=[None, input_size, 1], name ='input')
 
-    network = fully_connected(network, 128, activation='relu')
-    network = dropout(network, 0.8)
-    network = fully_connected(network, 256, activation='relu')
-    network = dropout(network, 0.8)
-    network = fully_connected(network, 512, activation='relu')
-    network = dropout(network, 0.8)
-    network = fully_connected(network, 246, activation='relu')
-    network = dropout(network, 0.8)
-    network = fully_connected(network, 128, activation='relu')
-    network = dropout(network, 0.8)
-    network = fully_connected(network, 2, activation='softmax')
-    network = regression(network, optimizer='adam', learning_rate=LR, loss="categorical_crossentropy", name='targets')
-    model = tflearn.DNN(network, tensorboard_dir='log')
+    model.add(Dense(units=128, activation='relu', input_shape=(input_size,)))
+    model.add(Dropout(0.8))
+    model.add(Dense(units=256, activation='relu'))
+    model.add(Dropout(0.8))
+    model.add(Dense(units=512, activation='relu'))
+    model.add(Dropout(0.8))
+    model.add(Dense(units=246, activation='relu'))
+    model.add(Dropout(0.8))
+    model.add(Dense(units=128, activation='relu'))
+    model.add(Dropout(0.8))
+    model.add(Dense(units=2, activation='softmax'))
+    model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
     return model
 
 def train_model(training_data, model=False):
 
-    X = np.array([i[0] for i in training_data]).reshape(-1,len(training_data[0][0]),1)
+    X = np.array([i[0] for i in training_data])
     y = [i[1] for i in training_data]
+    print(X)
+    print(y)
 
     if not model:
+        print(len(X[0]))
         model = neural_network_model(input_size = len(X[0]))
 
-    model.fit({'input': X}, {'targets': y}, n_epoch=3, snapshot_step=500, show_metric=True)
+    model.fit(X, y, epochs=3, batch_size=32)
     return model
 model = train_model(initial_population())
 
@@ -171,9 +177,9 @@ for each_game in range(100):
 
     scores.append(score)
 
-#print('Average Score:',sum(scores)/len(scores))
-#print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
-#print(score_requirement)
+print('Average Score:',sum(scores)/len(scores))
+print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
+print(score_requirement)
 
 model = train_model(next_population(), model)
 #
